c38b437591fededb894b05dff843d7a3aedbef7e
Gerard Gomez <gerardgomez11@me.comgerardgomez11@me.comgerardgomez11@me.com>
2025-01-26 16:28:21 -0500

    Initial Commit

---
+++ DiceSplitter/DiceSplitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceSplitterApp.swift
+//  DiceSplitter
+//
+//  Created by Gerard Gomez on 1/26/25.
+//
+
+import SwiftUI
+
+@main
+struct DiceSplitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DiceSplitterTests/DiceSplitterTests.swift
@@ -0,0 +1,16 @@
+//
+//  DiceSplitterTests.swift
+//  DiceSplitterTests
+//
+//  Created by Gerard Gomez on 1/26/25.
+//
+
+import Testing
+
+struct DiceSplitterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
